038c4b528c765c50dd4fba437a5a333cdc22f2e2
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-01-29 04:45:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'ogonzaleznyc';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  
+  //return '';
+  
+  return '489ac3f778101b4b143134d6b1420acda00e75cd';
+  
+  //return '';
 }
 
 function forkRepo() {
+  
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  
+  //POST /repos/:owner/:repo/forks
+  //fetch(`${baseURL}/repos/${repo}/forks`, 
+  fetch(`${baseURL}/repos/${repo}/forks`, 
+    {method: 'POST', headers:
+      {Authorization: `token ${getToken()}`
+      }
+    }).then(response => response.json())
+    .then( json => console.log(json));
+    //.then(json => showResults(json));
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  
+  //$('#results').innerHTML = json;
+  //document.getElementById('results').innerHTML = json;
+  
 }
 
 function createIssue() {
